9aed1b0f03b7350ff6c5da9466348ea7bbd4d42a
Dan Le <dhl1337@gmail.com>
2017-02-19 22:51:58 -0700

    add class level properties

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: PropTypes.object
+    };
+
     render() {
-      console.log(this.props.authenticated);
+      console.log(this.context);
       return <ComposedComponent {...this.props}/>
     }
   }
